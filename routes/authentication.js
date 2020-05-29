bae7abb375b02866cd66413c0636b0dfe3238042
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-29 12:08:56 +0100

    update profile

--- routes/authentication.js
+++ routes/authentication.js
@@ -29,7 +29,7 @@ router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
-router.post('/sign-up', uploader.single('image'), (req, res, next) => {
+router.post('/sign-up', (req, res, next) => {
   const { name, email, password, phoneNumber } = req.body;
   bcryptjs
     .hash(password, 10)
